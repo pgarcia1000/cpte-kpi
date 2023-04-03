f87ec0d55a0408faea77c4ea9d9c4ad8655bed56
Pedro G <pgarcia1000@gmail.com>
2023-04-03 00:10:24 -0600

    Eliminacion

--- resources/views/layouts/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer text-sm">
-    <div>
-        <a href="/">Bilas</a>.
-        Copyright &copy; {{ date('Y') }}
-    </div>
-    <div class="ms-auto">Bootstrap Admin Template</div>
-</footer>
